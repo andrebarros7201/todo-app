3ab3980c0e94be5445ccaa470b7e403861664e67
André Barros <andrebarros7201@gmail.com>
2024-09-05 16:20:26 +0100

    create database context

---
+++ backend/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class Context: DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
