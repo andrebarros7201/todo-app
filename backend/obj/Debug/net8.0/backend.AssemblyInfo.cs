d49e8dadabd81887b8ba3837501cf44465be544f
André Barros <andrebarros7201@gmail.com>
2024-09-05 16:21:29 +0100

    delete weatherforecast files

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c10294396aeaeb876095d81be6541a0ed61815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab3980c0e94be5445ccaa470b7e403861664e67")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
