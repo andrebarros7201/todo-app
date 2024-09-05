88c10294396aeaeb876095d81be6541a0ed61815
André Barros <andrebarros7201@gmail.com>
2024-09-05 16:12:24 +0100

    create Todo model

---
+++ backend/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Priority { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
